aa016336090af60b296a563c553af9472ccc1e71
KareemDono <kareemjumaa16@gmail.com>
2023-05-31 13:36:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,79 +21,71 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.edr434m.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-//users
-app.use("/users", userRoutes);
-app.get("/users", userRoutes.getUsers);
-app.post("/users", userRoutes.createUser);
-app.get("/users/:id", userRoutes.getUserById);
-app.put("/users/:id", userRoutes.updateUser);
-app.delete("/users/:id", userRoutes.deleteUser);
+// Add "/api/" prefix to all routes
+app.use("/api/users", userRoutes);
+app.get("/api/users", userRoutes.getUsers);
+app.post("/api/users", userRoutes.createUser);
+app.get("/api/users/:id", userRoutes.getUserById);
+app.put("/api/users/:id", userRoutes.updateUser);
+app.delete("/api/users/:id", userRoutes.deleteUser);
 
-//products
-app.use("/products", productRoutes);
-app.get("/products", productRoutes.getProducts);
-app.post("/products", productRoutes.createProduct);
-app.get("/products/:id", productRoutes.getProductById);
-app.put("/products/:id", productRoutes.updateProduct);
-app.delete("/products/:id", productRoutes.deleteProduct);
+app.use("/api/products", productRoutes);
+app.get("/api/products", productRoutes.getProducts);
+app.post("/api/products", productRoutes.createProduct);
+app.get("/api/products/:id", productRoutes.getProductById);
+app.put("/api/products/:id", productRoutes.updateProduct);
+app.delete("/api/products/:id", productRoutes.deleteProduct);
 
-//saved products
-app.use("/saved-products", savedProductRoutes);
-app.get("/saved-products", savedProductRoutes.getSavedProducts);
-app.post("/saved-products", savedProductRoutes.createSavedProduct);
-app.get("/saved-products/:id", savedProductRoutes.getSavedProductById);
-app.put("/saved-products/:id", savedProductRoutes.updateSavedProduct);
-app.delete("/saved-products/:id", savedProductRoutes.deleteSavedProduct);
+app.use("/api/saved-products", savedProductRoutes);
+app.get("/api/saved-products", savedProductRoutes.getSavedProducts);
+app.post("/api/saved-products", savedProductRoutes.createSavedProduct);
+app.get("/api/saved-products/:id", savedProductRoutes.getSavedProductById);
+app.put("/api/saved-products/:id", savedProductRoutes.updateSavedProduct);
+app.delete("/api/saved-products/:id", savedProductRoutes.deleteSavedProduct);
 
-//user type
-app.use("/user-types", userTypeRoutes);
-app.get("/user-types", userTypeRoutes.getUserTypes);
-app.post("/user-types", userTypeRoutes.createUserType);
-app.get("/user-types/:id", userTypeRoutes.getUserTypeById);
-app.put("/user-types/:id", userTypeRoutes.updateUserType);
-app.delete("/user-types/:id", userTypeRoutes.deleteUserType);
+app.use("/api/user-types", userTypeRoutes);
+app.get("/api/user-types", userTypeRoutes.getUserTypes);
+app.post("/api/user-types", userTypeRoutes.createUserType);
+app.get("/api/user-types/:id", userTypeRoutes.getUserTypeById);
+app.put("/api/user-types/:id", userTypeRoutes.updateUserType);
+app.delete("/api/user-types/:id", userTypeRoutes.deleteUserType);
 
-//orders
-app.use("/orders", orderRoutes);
-app.get("/orders", orderRoutes.getOrders);
-app.post("/orders", orderRoutes.createOrder);
-app.get("/orders/:id", orderRoutes.getOrderById);
-app.put("/orders/:id", orderRoutes.updateOrder);
-app.delete("/orders/:id", orderRoutes.deleteOrder);
+app.use("/api/orders", orderRoutes);
+app.get("/api/orders", orderRoutes.getOrders);
+app.post("/api/orders", orderRoutes.createOrder);
+app.get("/api/orders/:id", orderRoutes.getOrderById);
+app.put("/api/orders/:id", orderRoutes.updateOrder);
+app.delete("/api/orders/:id", orderRoutes.deleteOrder);
 
-//manufacturers
-app.use("/manufacturer", manufacturerRoutes);
-app.get("/manufacturer", manufacturerRoutes.getManufacturers);
-app.post("/manufacturer", manufacturerRoutes.createManufacturer);
-app.get("/manufacturer/:id", manufacturerRoutes.getManufacturerById);
-app.put("/manufacturer/:id", manufacturerRoutes.updateManufacturer);
-app.delete("/manufacturer/:id", manufacturerRoutes.deleteManufacturer);
+app.use("/api/manufacturer", manufacturerRoutes);
+app.get("/api/manufacturer", manufacturerRoutes.getManufacturers);
+app.post("/api/manufacturer", manufacturerRoutes.createManufacturer);
+app.get("/api/manufacturer/:id", manufacturerRoutes.getManufacturerById);
+app.put("/api/manufacturer/:id", manufacturerRoutes.updateManufacturer);
+app.delete("/api/manufacturer/:id", manufacturerRoutes.deleteManufacturer);
 
-//computer
-app.use("/computer", computerRoutes);
-app.get("/computer", computerRoutes.getComputers);
-app.post("/computer", computerRoutes.createComputer);
-app.get("/computer/:id", computerRoutes.getComputerById);
-app.put("/computer/:id", computerRoutes.updateComputer);
-app.delete("/computer/:id", computerRoutes.deleteComputer);
+app.use("/api/computer", computerRoutes);
+app.get("/api/computer", computerRoutes.getComputers);
+app.post("/api/computer", computerRoutes.createComputer);
+app.get("/api/computer/:id", computerRoutes.getComputerById);
+app.put("/api/computer/:id", computerRoutes.updateComputer);
+app.delete("/api/computer/:id", computerRoutes.deleteComputer);
 
-//category
-app.use("/category", categoryRoutes);
-app.get("/category", categoryRoutes.getCategories);
-app.post("/category", categoryRoutes.createCategory);
-app.get("/category/:id", categoryRoutes.getCategoryById);
-app.put("/category/:id", categoryRoutes.updateCategory);
-app.delete("/category/:id", categoryRoutes.deleteCategory);
+app.use("/api/category", categoryRoutes);
+app.get("/api/category", categoryRoutes.getCategories);
+app.post("/api/category", categoryRoutes.createCategory);
+app.get("/api/category/:id", categoryRoutes.getCategoryById);
+app.put("/api/category/:id", categoryRoutes.updateCategory);
+app.delete("/api/category/:id", categoryRoutes.deleteCategory);
 
-//computer type
-app.use("/computer-type", computerTypeRoutes);
-app.get("/computer-type", computerTypeRoutes.getComputerTypes);
-app.post("/computer-type", computerTypeRoutes.createComputerType);
-app.get("/computer-type/:id", computerTypeRoutes.getComputerTypeById);
-app.put("/computer-type/:id", computerTypeRoutes.updateComputerType);
-app.delete("/computer-type/:id", computerTypeRoutes.deleteComputerType);
+app.use("/api/computer-type", computerTypeRoutes);
+app.get("/api/computer-type", computerTypeRoutes.getComputerTypes);
+app.post("/api/computer-type", computerTypeRoutes.createComputerType);
+app.get("/api/computer-type/:id", computerTypeRoutes.getComputerTypeById);
+app.put("/api/computer-type/:id", computerTypeRoutes.updateComputerType);
+app.delete("/api/computer-type/:id", computerTypeRoutes.deleteComputerType);
 
-app.use("/", (req, res) => {
+app.use("/api", (req, res) => {
   res.send('Welcome to the server');
 });
 
